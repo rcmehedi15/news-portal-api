49d54cbd95d7056aca77177eec734228f05f16c6
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-03-01 21:19:13 +0600

    heading finish

--- js/portal.js
+++ js/portal.js
@@ -7,6 +7,19 @@ const showCategories = data => {
     // capture categories container to append all the category links
     const categoriesContainer = document.getElementById('categories-container');
     data.news_category.forEach(singleCategory =>{
-        categoriesContainer.innerHTML += `<a class="nav-link" href="#">${singleCategory.category_name}</a>`
+        categoriesContainer.innerHTML += `<a class="nav-link" href="#" onclick="fetchCategoriesNews('${singleCategory.category_id}')" >${singleCategory.category_name}</a>`
+    
+        // let linkContainer = document.createElement('p'); --- 2nd styles
+        // linkContainer.innerHTML = `<a class="nav-link" href="#">${singleCategory.category_name}</a>`
+        // categoriesContainer.appendChild(linkContainer)
     })
+}
+
+// -----------  fetch all news available in a category (2)
+
+const fetchCategoriesNews = category_id => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
 }
