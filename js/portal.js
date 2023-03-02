a86e23ff4da015cfd3a4e3966f08ee7533c4a84c
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-03-02 10:30:32 +0600

    div create , image, title ,details show finish

--- js/portal.js
+++ js/portal.js
@@ -28,4 +28,26 @@ const showAllNews = (data, category_name) => {
     console.log(data, category_name);
     document.getElementById('news-count').innerText = data.data.length;
     document.getElementById('category-name').innerText = category_name;
-}
+      const newsContainer = document.getElementById('all-news');
+      newsContainer.innerHTML = ""
+    data.data.forEach((singleNews) => {
+       const card = document.createElement('div');
+       card.classList.add('card','mb-3');
+       card.innerHTML = `
+       <div class="row g-0">
+       <div class="col-md-4">
+         <img src="${singleNews.image_url}" class="img-fluid rounded-start" alt="...">
+       </div>
+       <div class="col-md-8">
+         <div class="card-body">
+           <h5 class="card-title">${singleNews.title}</h5>
+           <p class="card-text">${singleNews.details.slice(0,200)}...</p>
+           <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+         </div>
+       </div>
+     </div>
+       `;
+       newsContainer.appendChild(card);
+       
+    })
+};
